f5088cf7db39f7cb9e1448e80899395124594ddb
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-30 07:44:50 -0700

    [ADD] Configuracion basica del back

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.saludar = void 0;
+var saludar = function () {
+    console.log('Hola Server.ts');
+};
+exports.saludar = saludar;
+//# sourceMappingURL=server.js.map
